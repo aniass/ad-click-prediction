86ac5f6260ebe59a0d7e9a0999e5904882af360d
Aniass <a.staskiewicz4@gmail.com>
2023-12-20 18:53:45 +0100

    code refactor

--- prediction_model.py
+++ prediction_model.py
@@ -11,9 +11,9 @@ URL = '\Data\Ad_Click_prediciton_test.csv'
 label_encoder = LabelEncoder()
 
 
-def load_model():
+def load_model(model_path):
     '''Loading pretrained model'''
-    with open(MODELSPATH, 'rb') as file:
+    with open(model_path, 'rb') as file:
         model = load(file)
         return model
     
@@ -22,17 +22,17 @@ def clean_data(df):
     '''Deleting missing data, feature engineering for date time features'''
     df = df.dropna(subset=['gender','age_level', 'user_group_id', 'user_depth'])
     df['DateTime'] = pd.to_datetime(df['DateTime'], errors='coerce')
-    df = df.assign(hour = df.DateTime.dt.hour,
-                   day_of_week = df.DateTime.apply(lambda x: x.dayofweek),)
+    df = df.assign(hour=df.DateTime.dt.hour,
+                   day_of_week=df.DateTime.apply(lambda x: x.dayofweek),)
     return df
 
 
 def data_transformation(data):
     '''Filling missing values and convert non numeric values'''
     df = clean_data(data)
-    df = df.assign(city_development_index = df.city_development_index.fillna('0'),
-                   product_category_2 = df.product_category_2.fillna("0"),
-                   gender = df.gender.map({'Male' : 0, 'Female' : 1}),)
+    df = df.assign(city_development_index=df.city_development_index.fillna('0'),
+                   product_category_2=df.product_category_2.fillna("0"),
+                   gender=df.gender.map({'Male' : 0, 'Female' : 1}),)
     df['product'] = label_encoder.fit_transform(df['product'])
     data = df.drop(['session_id','user_id', 'DateTime'], axis=1) 
     return data
@@ -48,7 +48,7 @@ def read_data(path):
 def get_prediction(test):
     '''Generating predictions from test data'''
     test_X = np.array(test)
-    model = load_model()
+    model = load_model(MODELSPATH)
     predicted = model.predict(test_X)
     test['click_ prediction'] = predicted
     print(test.head())
