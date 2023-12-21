2915149d77dfde5125d18e9baa621eade0a22f36
Aniass <a.staskiewicz4@gmail.com>
2023-12-21 20:25:05 +0100

    code refactor

--- prediction_model.py
+++ prediction_model.py
@@ -3,24 +3,24 @@ import numpy as np
 from sklearn.preprocessing import LabelEncoder
 from joblib import load
 import warnings
-warnings.simplefilter('ignore')
 
+warnings.simplefilter('ignore')
 
 MODELSPATH = '\models\adaboost_model.pkl'
 URL = '\Data\Ad_Click_prediciton_test.csv'
-label_encoder = LabelEncoder()
+LABEL_ENCODER = LabelEncoder()
 
 
 def load_model(model_path):
-    '''Loading pretrained model'''
+    '''Load pretrained model'''
     with open(model_path, 'rb') as file:
         model = load(file)
         return model
     
 
 def clean_data(df):
-    '''Deleting missing data, feature engineering for date time features'''
-    df = df.dropna(subset=['gender','age_level', 'user_group_id', 'user_depth'])
+    '''Delete missing data, perform feature engineering for date time feature'''
+    df = df.dropna(subset=['gender','age_level','user_group_id','user_depth'])
     df['DateTime'] = pd.to_datetime(df['DateTime'], errors='coerce')
     df = df.assign(hour=df.DateTime.dt.hour,
                    day_of_week=df.DateTime.apply(lambda x: x.dayofweek),)
@@ -28,29 +28,29 @@ def clean_data(df):
 
 
 def data_transformation(data):
-    '''Filling missing values and convert non numeric values'''
+    '''Fill missing values and convert non numeric values'''
     df = clean_data(data)
     df = df.assign(city_development_index=df.city_development_index.fillna('0'),
                    product_category_2=df.product_category_2.fillna("0"),
-                   gender=df.gender.map({'Male' : 0, 'Female' : 1}),)
-    df['product'] = label_encoder.fit_transform(df['product'])
-    data = df.drop(['session_id','user_id', 'DateTime'], axis=1) 
+                   gender=df.gender.map({'Male':0,'Female':1}),)
+    df['product'] = LABEL_ENCODER.fit_transform(df['product'])
+    data = df.drop(['session_id','user_id','DateTime'], axis=1) 
     return data
 
 
 def read_data(path):
-    '''Function to read data'''
+    '''Read data and perform data transformation'''
     data = pd.read_csv(path)
     df = data_transformation(data)
     return df
 
 
 def get_prediction(test):
-    '''Generating predictions from test data'''
+    '''Generate predictions from test data'''
     test_X = np.array(test)
     model = load_model(MODELSPATH)
     predicted = model.predict(test_X)
-    test['click_ prediction'] = predicted
+    test['click_prediction'] = predicted
     print(test.head())
 
 
