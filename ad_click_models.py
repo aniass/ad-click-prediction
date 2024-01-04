6164818ad089b6f0ab7260aeb76b31a933386f6e
Aniass <a.staskiewicz4@gmail.com>
2024-01-04 19:36:00 +0100

    code refactor

--- ad_click_models.py
+++ ad_click_models.py
@@ -1,22 +1,20 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import StratifiedShuffleSplit
 from imblearn.pipeline import Pipeline as imbpipeline
 # Model packages
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import LinearSVC
-from sklearn.ensemble import AdaBoostClassifier
 from sklearn.metrics import f1_score
 import warnings
+
 warnings.simplefilter('ignore')
 
-
-URL = '\data\Ad_click_prediction_train.csv'
+URL = 'data/Ad_click_prediction_train.csv'
 label_encoder = LabelEncoder()
 
 
@@ -34,9 +32,9 @@ def data_transformation(data):
     df = clean_data(data)
     df = df.assign(city_development_index = df.city_development_index.fillna('0'),
                    product_category_2 = df.product_category_2.fillna("0"),
-                   gender = df.gender.map({'Male' : 0, 'Female' : 1}),)
+                   gender = df.gender.map({'Male':0, 'Female':1}),)
     df['product'] = label_encoder.fit_transform(df['product'])
-    data = df.drop(['session_id','user_id', 'DateTime'], axis=1) 
+    data = df.drop(['session_id','user_id','DateTime'], axis=1) 
     return data
 
 
@@ -49,7 +47,7 @@ def read_data(path):
 
 def splitting_data(data):
     '''Spliting data into train and test set'''
-    X = np.array(data.drop(['is_click'],1))
+    X = np.array(data.drop(['is_click'], 1))
     y = np.array(data['is_click'])
     skf = StratifiedShuffleSplit(n_splits=5, test_size=.25, random_state=0)
     for train_index, test_index in skf.split(X, y):
@@ -58,11 +56,11 @@ def splitting_data(data):
         return X_train, X_test, y_train, y_test
 
 
-def f_score(model):
+def f_score(model, X_test, y_test):
     """F1 score calculation"""
     y_pred = model.predict(X_test)
     score = f1_score(y_test, y_pred, average='weighted')
-    score = round(score,3)
+    score = round(score, 3)
     return score  
 
 
@@ -74,18 +72,19 @@ def train_models(X_train, X_test, y_train, y_test):
         LinearSVC(random_state=0),
         DecisionTreeClassifier(max_depth=2, criterion='entropy', random_state=20),
         RandomForestClassifier(max_depth=5, n_estimators=200, criterion='entropy', random_state=0),
-        AdaBoostClassifier(n_estimators=200 ,random_state=0)]
+        AdaBoostClassifier(n_estimators=200 ,random_state=0)
+    ]
      
     for classifier in classifiers:
         model = imbpipeline(steps=[('scaler', MinMaxScaler()),
                                     ('smote', SMOTE()),
                                     ('classifier', classifier)])
         model.fit(X_train, y_train)
-        score = f_score(model)
+        score = f_score(model, X_test, y_test)
         param_dict = {
                      'Model': classifier.__class__.__name__,
                      'F1 score': score
-                     }
+        }
         models = models.append(pd.DataFrame(param_dict, index=[0]))
         
     models.reset_index(drop=True, inplace=True)
